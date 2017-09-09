6c45c733082562b35eeedab5478ab734d1771679
Dylan <dj0759@gmail.com>
2017-09-09 16:44:52 -0700

    updated some pages

---
+++ src/components/frontPage.js
@@ -0,0 +1,43 @@
+import {Button, Jumbotron, Carousel, CarouselItem, Form, Col, FormGroup, FormControl, Checkbox, ControlLabel} from 'react-bootstrap';
+import React from 'react';
+
+export default class Index extends React.Component {
+    render(){
+        return(
+            <div>
+                <div id="jumbotron">
+                <Jumbotron>
+    <h1>Euro Travel</h1>
+    <p>The worlds finest travel accommodation accessory</p>
+
+    <p><Button bsStyle="primary">Learn more</Button></p>
+        </Jumbotron>
+        </div>
+
+        <Carousel>
+        <Carousel.Item>
+          <img width={900} height={500} alt="900x500" src="http://duntonhotsprings.com/files/7314/1344/7797/Echo-Cabin.jpg"/>
+          {/* <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width={900} height={500} alt="900x500" src="http://www.telegraph.co.uk/content/dam/Travel/galleries/travel/activityandadventure/The-worlds-most-beautiful-mountains/mountains-Kirkjufe_3374110a.jpg"/>
+          {/* <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width={900} height={500} alt="900x500" src="https://media.cntraveler.com/photos/58acc16ee85725558885abc8/16:9/w_1024,c_limit/tahiti-GettyImages-550870471.jpg"/>
+          <Carousel.Caption>
+            {/* <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+        </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,26 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-import {Button, Jumbotron, Form, Col, FormGroup, FormControl, Checkbox, ControlLabel} from 'react-bootstrap';
+import {Button, Jumbotron, Carousel, CarouselItem, Form, Col, FormGroup, FormControl, Checkbox, ControlLabel} from 'react-bootstrap';
 import Signin from './components/Signin';
 import Signup from './components/signup';
+import Dashboard from './components/dashboard';
+import Index from './components/frontPage';
 
 
 
-
-class App extends React.Component {
-    render(){
-        return(
-            <div>
-                <Jumbotron>
-    <h1>Euro Travel</h1>
-    <p>The worlds finest travel accommodation accessory</p>
-    <p><Button bsStyle="primary">Learn more</Button></p>
-        </Jumbotron>
-            </div>
-        )
-    }
-}
 
 class Routes extends React.Component { 
     render(){
@@ -32,11 +20,13 @@ class Routes extends React.Component {
                     <Button waves='red'><Link to="/" >Home</Link></Button>
                     <Button waves='red'><Link to="/signin" >Sign In</Link></Button>
                     <Button waves='red'><Link to="/signup" >Sign Up</Link></Button>
+                    <Button waves='red'><Link to="/dashboard" >Dashboard</Link></Button>
                     <br/>
                     <br/>
-            <Route exact path='/' component={App} />
+            <Route exact path='/' component={Index} />
             <Route path='/signin' component={Signin}/>
             <Route path='/signup' component={Signup}/>
+            <Route path='/dashboard' component={Dashboard}/>
             
             </div>
         </Router>
